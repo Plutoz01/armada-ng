83efc6ebb218b01f2b359291d88f324b8977d278
Angular CLI <angular-cli@angular.io>
2017-07-08 21:34:23 +0200

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { ArmadaNgPage } from './app.po';
+
+describe('armada-ng App', () => {
+  let page: ArmadaNgPage;
+
+  beforeEach(() => {
+    page = new ArmadaNgPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to ar!!');
+  });
+});
