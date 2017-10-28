635b3b0c0fb6d8f13f3447aa43af9f07b0e7d9f0
László Csatlós <plutoz01@gmail.com>
2017-10-28 09:13:17 +0200

    remove unused methods

--- src/app/examples/components/card/card.component.ts
+++ src/app/examples/components/card/card.component.ts
@@ -1,16 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component( {
 	selector: 'ar-ex-card',
 	templateUrl: './card.component.html',
 	styleUrls: [ './card.component.scss' ]
 } )
-export class ExampleCardComponent implements OnInit {
-
-	constructor() {
-	}
-
-	ngOnInit() {
-	}
-
+export class ExampleCardComponent {
 }
--- src/app/lib/components/card/card.component.ts
+++ src/app/lib/components/card/card.component.ts
@@ -1,16 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component( {
 	selector: 'ar-card',
 	templateUrl: './card.component.html',
 	styleUrls: [ './card.component.scss' ]
 } )
-export class CardComponent implements OnInit {
-
-	constructor() {
-	}
-
-	ngOnInit() {
-	}
-
+export class CardComponent {
 }
